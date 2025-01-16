09118c7baea15f5c7c9a3eeedc9ae92991fc2e4d
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2025-01-16 13:11:55 +0100

    add suspense

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { fetchUsersList } from "@/lib/usersApi";
 import { UsersCard } from "../components/usersCard/usersCard";
 import { UserDetails } from "../components/userDetails/userDetails";
@@ -5,9 +6,11 @@ import { UserDetails } from "../components/userDetails/userDetails";
 export default async function UsersPage() {
   const usersList = await fetchUsersList();
   return (
-    <div style={{ padding: "20px" }}>
-      <UsersCard usersList={usersList} />
-      <UserDetails />
-    </div>
+    <Suspense fallback={<span>Loading...</span>}>
+      <div style={{ padding: "20px" }}>
+        <UsersCard usersList={usersList} />
+        <UserDetails />
+      </div>
+    </Suspense>
   );
 }
